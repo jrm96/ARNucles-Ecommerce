725d42f52ba46d142908a61db51879415ade10c8
Josue Murillo <jrmurillo@Josues-Mac-mini.local>
2021-08-18 11:51:18 -0600

    Agregada autenticación OAuth

--- config/nodemailer.config.js
+++ config/nodemailer.config.js
@@ -2,18 +2,19 @@ const nodemailer = require("nodemailer");
 const dotenv = require('dotenv');
 dotenv.config();
 
-const user = process.env.EMAIL_USER;
-const pass = process.env.EMAIL_PASSWORD;
 const host = process.env.HOST;
 
 const transport = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
-  requireTLS: true,
+  port: 465,
+  secure: true,
   auth: {
-    user: user,
-    pass: pass,
+    type: 'OAuth2',
+    user: process.env.EMAIL_USER,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken: process.env.REFRESH_TOKEN,
+    accessToken: process.env.ACCESS_TOKEN,
   },
 });
 
